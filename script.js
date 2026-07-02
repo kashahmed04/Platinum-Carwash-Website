de4cdcc7b50a36327a9bd2eab2b032e948074e06
Kash <kash.ahmed84@gmail.com>
2026-07-02 11:46:20 -0400

    updated photos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 TODO:
 
-make sure favicons work across all browser types and devices
+make sure favicons work acgross all browser types and devices
 update readme / github when site is done
 make sure all photos are straight on website 
 make sure favicons work for all device types and sizes (apple, etc.)
